ac998d57ed9ca10ce27e384182ccc34be8c4668c
Александр Филиппов <alekfilippov@MacBook-Air-Aleksandr.local>
2023-05-03 14:24:22 +0300

    Remove test_db.py from index

--- test_db.py
+++
@@ -1,7 +0,0 @@
-import psycopg2
-
-
-def get_db_connection():
-    conn = psycopg2.connect(database="sarova", user="postgres", password="postgres",
-                            host="localhost", port="5432")
-    return conn
